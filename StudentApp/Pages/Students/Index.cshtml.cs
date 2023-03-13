635ffd2eeb7fd2cb44b3cbe1d9fb9acb3129bc14
Zubayer Bin Ayub <zubayer01913@gmail.com>
2023-03-13 23:49:30 +0600

    added student index page

---
+++ StudentApp/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentApp.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
